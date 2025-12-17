b6893153fecec2cc5c4dcdd10fceec8e93d26236
Jade <jade16work@gmail.com>
2025-12-17 10:48:21 +0900

    게임 초기 필드 데이터 선언

--- ConsoleApp3/ConsoleApp3/Program.cs
+++ ConsoleApp3/ConsoleApp3/Program.cs
@@ -1,23 +1,63 @@
-﻿using System;
+﻿// 소코반 구현 구조
+// 1. 초기 세팅
+// 2. 화면에 출력
+// 3. 사용자 입력
+// 4. 로직 수행 (이동, 폭탄 밀기)
+// 5. 2~4 반복
 
+using System;
 class Program
 {
+    private const char PLAYER = 'P';        // 플레이어,
+    private const char PLAYER_ON_GOAL = '@';// 플레이어가 목표지점 위에 있는 상태,
+    private const char BOMB = 'B';          // 폭탄,
+    private const char BOMB_ON_GOAL = '!';  // 폭탄이 목표지점 위에 있는 상태,
+    private const char GOAL = 'G';          // 목표지점,
+    private const char WALL = '#';          // 벽,
+    private const char EMPTY = ' ';         // 빈공간
+
+    private static char[,] map = new char[,] // 게임 필드(2차원 배열)
+    {
+        { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' },
+        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+        { '#', ' ', ' ', 'B', ' ', ' ', ' ', ' ', ' ', '#' },
+        { '#', ' ', ' ', ' ', 'P', 'G', ' ', ' ', ' ', '#' },
+        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+        { '#', ' ', ' ', 'B', ' ', ' ', 'G', ' ', ' ', '#' },
+        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+        { '#', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', '#' },
+        { '#', '#', '#', '#', '#', '#', '#', '#', '#', '#' }
+    };
+    
+    static Position _playerPos = new Position()
+    {
+        X = 4, 
+        Y = 4
+    };
+
+    private static int _moveCount = 0;
+    
     static void Main(string[] args)
     {
-        // 소코반 게임 만들기(콘솔)
-        // 절차지향 - 위에서 아래로 절차적으로 수행하도록 만드는 구조
-        // 함수를 적극 사용하자.
         
-        // 게임로직
-            // 1. 사용자 입력
-            // 2. 로직 수행
-            // 3. 화면에 출력
-            
-        // 소코반 구현 구조
-            // 1. 초기 세팅
-            // 2. 화면에 출력
-            // 3. 사용자 입력
-            // 4. 로직 수행 (이동, 폭탄 밀기)
-            // 5. 2~4 반복
     }
+
+    static void PrintMap()
+    {
+        for (int i = 0; i < map.GetLength(0); i++)
+        {
+            for (int j = 0; j < map.GetLength(1); j++)
+            {
+                Console.Write(map[i, j]);
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
+public struct Position
+{
+    public int X;
+    public int Y;
 }
